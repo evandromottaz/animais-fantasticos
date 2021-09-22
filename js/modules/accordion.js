36838efa58e7ae125324d62382a38e3a8f71e55f
Cantaloop Comunicação <https://github.com/evandromottaz/site-animais-fantastico.git>
2021-09-22 11:05:55 -0300

    Accordion transformado em classe

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,16 +1,27 @@
-export default function initAccordion() {
-    const dt = document.querySelectorAll('[data-anime="accordion"] dt')
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list)
+    this.activeClass = 'ativo'
+  }
 
-    function accordionActive() {
-        this.classList.toggle('ativo')
-        this.nextElementSibling.classList.toggle('ativo')
+  toggleAccordion(item) {
+      item.classList.toggle(this.activeClass)
+      item.nextElementSibling.classList.toggle(this.activeClass)
+  }
+
+  // adiciona os eventos ao accordion
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener('click', () => this.toggleAccordion(item));
+    })
+  }
+
+  // iniciar função
+  init() {
+    if(this.accordionList.length) {
+      // ativar primero item
+      this.toggleAccordion(this.accordionList[0])
+      this.addAccordionEvent();
     }
-    if(dt.length) {
-        dt[0].classList.add('ativo');
-        dt[0].nextElementSibling.classList.add('ativo');
-    
-        dt.forEach((item) => {
-            item.addEventListener('click', accordionActive)
-        })
-    }
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAnimaScroll from "./modules/anima-scroll.js";
-import initAccordion from "./modules/accordion.js";
+import Accordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
@@ -8,11 +7,14 @@ import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from './modules/funcionamento.js'
+import initAnimaScroll from "./modules/anima-scroll.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]')
 scrollSuave.init();
-initAnimaScroll();
-initAccordion();
+
+const accordion = new Accordion('[data-anime="accordion"] dt')
+accordion.init()
+
 initTabNav();
 initModal();
 initTooltip();
@@ -20,4 +22,5 @@ initDropdownMenu();
 initMenuMobile();
 initAnimaNumeros();
 initFuncionamento();
+initAnimaScroll();
 
