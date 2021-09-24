cf87e30ab14ef899ef44055e0475ee9113f990a8
Cantaloop Comunicação <https://github.com/evandromottaz/site-animais-fantastico.git>
2021-09-24 10:15:03 -0300

    modal transformado em classe

--- js/modules/tabnav.js
+++ js/modules/tabnav.js
@@ -24,8 +24,7 @@ export default class TabNav {
 
   init() {
     if(this.tabMenu.length && this.tabContent.length) {
-      // Ativar primeiro item
-      this.activeTab(0);
+      this.activeTab(0); // Ativar primeiro item
       this.addTabNavEvent();
     }
   }
