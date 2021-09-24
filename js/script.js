102e7eb793fe87a3f64fd04075977073d25e45d3
Cantaloop Comunicação <https://github.com/evandromottaz/site-animais-fantastico.git>
2021-09-24 12:42:48 -0300

    fetch-animais-refatorado

--- js/script.js
+++ js/script.js
@@ -5,10 +5,9 @@ import Modal from "./modules/modal.js";
 import Tooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from './modules/funcionamento.js'
+import fetchAnimais from "./modules/fetch-animais.js";
 import initAnimaScroll from "./modules/anima-scroll.js";
-import initFetchAnimais from "./modules/fetch-animais.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]')
 scrollSuave.init();
@@ -29,4 +28,5 @@ initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
 initAnimaScroll();
-initFetchAnimais();
+
+fetchAnimais('../../animaisapi.json','.numeros-grid');
